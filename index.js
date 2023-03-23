d1caa5e86d86bc7428d4a647eb4edc416b7967b4
Ajay-Dhangar <99037494+Ajay-Dhangar@users.noreply.github.com>
2023-03-23 19:21:02 +0530

    update app

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 let pera1 = document.getElementById("pera1");
 let pera2 = document.getElementById("pera2");
+let timeText = document.getElementById("timeText");
+let greetText = document.getElementById("greetText");
 function function1() {
   pera1.innerHTML = `The message and Image will change according to the time`;
   pera1.style.backgroundColor = "#fff";
   pera1.style.borderRadius = "10px";
+  pera1.style.height = "100%";
+  pera1.style.padding = "8px";
 }
 function function2() {
   pera2.innerHTML = `Time will be updated here after clicking on  "Set Alarm"`;
   pera2.style.backgroundColor = "#fff";
   pera2.style.borderRadius = "10px";
+  pera2.style.height = "88px";
+  pera2.style.padding = "8px";
 }
 function showTime() {
   var showDisplayTime = new Date();
@@ -19,16 +25,22 @@ function showTime() {
   var hr = showDisplayTime.getHours();
 
   // for image
-  if (hours > 8 && hours < 16) {
-    image = "./img/skyline-daytime.jpg"; // 8:00AM - 4:00PM
+  if (hours > 9 && hours < 16) {
+    image = "./img/Group 5183.png"; // 9:00AM - 4:00PM
+    timeText.innerHTML = `GOOD AFTERNOON !! TAKE SOME SLEEP`;
+    greetText.innerHTML = `LET'S HAVE SOME LUNCH !!`;
   } else {
     if (hours > 16 && hours < 19) {
-      image = "./img/evening.jpg"; // 4:00PM - 7:00PM
+      image = "./img/evening.png"; // 4:00PM - 7:00PM
+      timeText.innerHTML = `GOOD EVENING !!`;
+      greetText.innerHTML = `STOP YAWNING, GET SOME TEA.. ITS JUST EVENING!`;
     } else {
-      if (5 <= hours && hours <= 8) {
-        image = "./img/Component.png"; // 5:00AM - 8:00AM
+      if (5 <= hours && hours <= 9) {
+        image = "./img/Component.png"; // 5:00AM - 9:00AM
       } else {
         image = "./img/night.png"; // 7:00PM - 5:00AM
+        timeText.innerHTML = `GOOD NIGHT !!`;
+        greetText.innerHTML = `CLOSE YOUR EYES AND GO TO SLEEP`;
       }
     }
   }
